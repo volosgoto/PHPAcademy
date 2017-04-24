33bea142a67c7c8e1d87db028e344e09b118cbb9
andrey <volosgoto@yahoo.com>
2017-04-24 14:34:31 +0300

    Task_26 added.

---
+++ be/php_basics_tasks/task_26.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task 26</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+
+	<?php
+/*
+<p>26. Объявите константу DAYS_COUNT равную 7 и константу MONTH_COUNT равную 12 двумя разными способами объявления констант.</p>
+*/
+
+define('DAYS_COUNT', 7);
+echo DAYS_COUNT . '<br>';
+const MONTH_COUNT = 12;
+echo MONTH_COUNT;
+
+
+	?>
+</body>
+</html>
---
+++ be/php_basics_tasks/task_27.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task 27</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+
+	<?php
+/*
+*/
+
+
+	?>
+</body>
+</html>
