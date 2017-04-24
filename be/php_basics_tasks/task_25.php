f3dc78c99795054fed4c31c4f96e7dc77094c5a2
andrey <volosgoto@yahoo.com>
2017-04-24 12:52:00 +0300

    Task_25 added.

---
+++ be/php_basics_tasks/task_25.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task 25</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+	<?php
+/*
+
+
+	Все, что находится между открывающим и закрывающим
+php-тэгами – код. Варианты тэгов:
+<?php
+код РНР //Рекомендуется к использованию.
+?>
+
+<?
+код РНР
+?>
+
+Для этого тэга параметр "short_open_tag" в php.ini
+должен быть "on".
+Могут быть проблемы с распознаванием XML.
+*/
+	?>
+</body>
+</html>
