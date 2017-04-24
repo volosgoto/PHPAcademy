a595f70dcd492b21825ea8a7260bbc2584cf16de
andrey <volosgoto@yahoo.com>
2017-04-24 09:13:56 +0300

    Task_03 addet

---
+++ be/php_basics_tasks/task_03.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task 3</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+		<?php
+//<p>3. Выведите с помощью echo фразу "Меня зовут: %ваше_имя%", например: "Меня зовут: Иннокентий".</p>
+		$userName = 'Andrey';
+		$age = 34;
+		echo "Меня зовут: $userName", "<br />";
+		echo "Мне $age лет";
+		?>
+</body>
+</html>
