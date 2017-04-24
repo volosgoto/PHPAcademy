08bdd9d2c646bc8230ed781eb78d6a698018711e
andrey <volosgoto@yahoo.com>
2017-04-24 11:32:32 +0300

    Task_16 Calculator added

---
+++ be/php_basics_tasks/task_16.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task 15</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+	<?php
+/*
+<p>16. Создайте две переменные $a и $b, значения которых будут числами. Выведите максимальное число из этих двух.</p>
+	 */
+		$a = 0;
+		$b = 5.5;
+	echo "Максимальное между {$a} и {$b} число: ";
+	echo ($a > $b) ? $a : $b;
+ 
+
+	?>
+</body>
+</html>
