745e62e4bc71b0181fc8d6cab54a3dd9032babad
andrey <volosgoto@yahoo.com>
2017-04-24 11:37:11 +0300

    Task_16 max $a and $b

--- be/php_basics_tasks/task_16.php
+++ be/php_basics_tasks/task_16.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Task 15</title>
+	<title>Task 16</title>
 	<meta charset="UTF-8">
 	<meta name="keywords" content="php_basics_tasks">
 	<meta name="description" content="php_basics_tasks">
@@ -12,12 +12,10 @@
 /*
 <p>16. Создайте две переменные $a и $b, значения которых будут числами. Выведите максимальное число из этих двух.</p>
 	 */
-		$a = 0;
-		$b = 5.5;
+	$a = 0;
+	$b = 5.5;
 	echo "Максимальное между {$a} и {$b} число: ";
 	echo ($a > $b) ? $a : $b;
- 
-
 	?>
 </body>
 </html>
