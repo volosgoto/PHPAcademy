6b1536585581421d770e553b0cc04e36a1a3bf07
andrey <volosgoto@yahoo.com>
2017-04-24 09:06:55 +0300

    Task_02 addet

---
+++ be/php_basics_tasks/task_02.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Форма анкети</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+		<?php
+		/*<p>2. Создайте переменную age и присвойте ей значение, содержащее ваш возраст, например 20.</p>*/
+
+			// $userName = 'Andrey';
+			$age = 34;
+		?>
+</body>
+</html>
