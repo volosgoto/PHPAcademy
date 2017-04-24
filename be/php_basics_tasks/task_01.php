2e6cad9c7012431d43da59970ae8bdcf6b33cbb6
andrey <volosgoto@yahoo.com>
2017-04-24 08:53:11 +0300

    general folder for HW ceated. Task 1 addet

---
+++ be/php_basics_tasks/task_01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Форма анкети</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="php_basics_tasks">
+	<meta name="description" content="php_basics_tasks">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+		<?php
+			/*<p>1. Создайте переменную name и присвойте ей значение, содержащее ваше имя, например "Иннокентий".</p>*/
+
+			$userName = 'Andrey';
+		?>
+</body>
+</html>
