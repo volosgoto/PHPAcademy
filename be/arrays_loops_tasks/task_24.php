fa62dd39e249d0593b8adf4278aea00c6ceb4c48
andrey <volosgoto@yahoo.com>
2017-04-25 16:37:45 +0300

    Task_24 added

---
+++ be/arrays_loops_tasks/task_24.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>task 24</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="array_loops">
+	<meta name="description" content="array_loops">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+
+<?php
+/*
+<p>24. Вам нужно разработать программу, которая считала бы количество вхождений
+какой­нибуть выбранной вами цифры в выбранном вами числе. Например: цифра 5 в числе
+442158755745 встречается 4 раза.</p>
+ */
+$str=22334455;
+echo substr_count($str, 4);
+?>
+
+</body>
+</html>
