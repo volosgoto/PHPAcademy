744c9b318b9b52d13909f06a44831daaf17000d8
andrey <volosgoto@yahoo.com>
2017-04-25 15:24:08 +0300

    Task_16 added

---
+++ be/arrays_loops_tasks/task_16.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>task 15</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="array_loops">
+	<meta name="description" content="array_loops">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+
+<?php
+/*
+<p>16. Дан массив с элементами 1, 2, 3, 4, 5, 6, 7, 8, 9. С помощью цикла foreach и оператора if
+выведите на экран столбец элементов массива, как показано на картинке.
+1, 2, 3
+4, 5, 6
+7, 8, 9</p>
+ */
+
+	$arr = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	foreach ($arr as $key => $value) {
+		++$key;
+		if ($key % 3 == 0) {
+				echo "$key";
+				echo '<br>';
+			} else {
+				echo "$key" . ', ';
+			}
+
+		}
+
+?>
+
+</body>
+</html>
