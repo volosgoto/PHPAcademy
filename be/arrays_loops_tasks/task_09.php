fbd80f039cf3c88e5800619871ed328e01db6956
andrey <volosgoto@yahoo.com>
2017-04-25 12:41:07 +0300

    Task_09 added

---
+++ be/arrays_loops_tasks/task_09.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>task 9</title>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="array_loops">
+	<meta name="description" content="array_loops">
+	<meta name="author" content="Volosovich Andrey">
+</head>
+<body>
+<?php
+/*
+<p>9. Выведите столбец чисел от 1 до 100.</p> 
+*/
+	$i = 0;
+	for ($i=1; $i <= 100; $i++) {
+		echo $i . '<br>';
+	}
+?>
+
+</body>
+</html>
