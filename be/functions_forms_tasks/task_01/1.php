bf2681929fa1dafd17951833f0ea6b9be59fa5e5
andrey <volosgoto@yahoo.com>
2017-05-01 22:46:06 +0300

    task_01. upd

--- be/functions_forms_tasks/task_01/1.php
+++ be/functions_forms_tasks/task_01/1.php
@@ -1,10 +1,32 @@
 <?php 
-	$varA = $_GET['varA'];
-	$varB = $_GET['varB'];
+	
+/*
+<p>1. Создать форму с двумя элементами textarea. При отправке формы скрипт должен выдавать только те слова, которые есть и в первом и во втором поле ввода.
+Реализацию это логики необходимо поместить в функцию getCommonWords($a, $b), которая будет возвращать массив с общими словами.
+</p>
 
+*/
+
+
+	$varA = explode(' ', $_GET['varA']);
+	$varB = explode(' ', $_GET['varB']);
+
+	
+// добавить проверку на входные данные
 	if ($varA == $varB) {
 		echo "Равны";
 	} else {
 		echo "Не равны";
 	}
+echo "<br>";
+
+var_dump($_GET['varA']);
+echo "<br>";
+
+	$result = array_intersect($varA, $varB); 
+	foreach ($result as $value) {
+		echo "$value";
+		echo "<br>";
+	}
+
 ?>
